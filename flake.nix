f40dddfd14f6a037217b31f37b53592928c5cead
Vivek Booshan <vivek@Viveks-MacBook-Pro.local>
2025-04-22 12:32:30 -0400

    remove excess

--- flake.nix
+++ flake.nix
@@ -20,43 +20,10 @@
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 
-	  # Hardware (optional for nix-darwin)
-	  # hardware.url = "github:nixos/nixos-hardware";
-
 	  # Nix-homebrew
 	  nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
 
 	};
-
-# 	outputs = inputs@{ self, nixpkgs, home-manager, nix-darwin, nix-homebrew, ... }: {
-# 	 # nix-darwin configuration entrypoint
-# 	 # Available through 'darwin-rebuild --flake .#your-hostname'
-
-# 		darwinConfigurations = {
-# 			  m1mac       = nix-darwin.lib.darwinSystem {
-# 				system      = "aarch64-darwin"; # Adjust the system type if needed
-# 				specialArgs = { inherit  inputs  self;  }; # Pass flake inputs to our config
-# 				modules     = [ 
-# 				  ./nix-darwin/configuration.nix
-# 				];
-# 			};
-# 		};
-
-# 		# home-manager configuration entrypoint
-# 		# Available through 'home-manager --flake .#your-username@your-hostname'
-# 		homeConfigurations = {
-# 			"vivek@m1mac" = home-manager.lib.homeManagerConfiguration {
-# 				# pkgs            = nixpkgs.legacyPackages.aarch64-darwin; # Adjust for Darwin
-# 				pkgs              = import nixpkgs { system = "aarch64-darwin"; };
-# 				extraSpecialArgs  = { inherit inputs; }; # Pass flake inputs to our config
-# 				modules           = [ 
-# 					./home-manager/home.nix 
-# 				];
-# 			};
-# 		};
-# 	};
-
-# }
 
 	outputs = inputs@{ self, nixpkgs, home-manager, nix-darwin, nix-homebrew, ... }: let
 		inherit (self) outputs;
